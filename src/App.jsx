146b9550857a16fe6fc97d6d5c9919ec5014508b
Korsite <hsr49rsh@gmail.com>
2025-02-18 02:05:43 -0600

    changes2

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,16 @@ import {Bebidas, ComidaChina, ComidaMexicana, ComidaRapida, Ensaladas, Paquetes}
 
 export const App = () => {
     return (
-        <>
-            <Routes>
-                <Route path="/dark-kitchen" element={<Inicio/>}/>
-                <Route path='dark-kitchen/Bebidas' element={ <Bebidas /> }/>
-                <Route path='dark-kitchen/ComidaChina' element={ <ComidaChina /> }/>
-                <Route path='dark-kitchen/ComidaMexicana' element={ <ComidaMexicana /> }/>
-                <Route path='dark-kitchen/ComidaRapida' element={ <ComidaRapida /> }/>
-                <Route path='dark-kitchen/Ensaladas' element={ <Ensaladas /> }/>
-                <Route path='dark-kitchen/Paquetes' element={ <Paquetes /> }/>
+        <Routes>
+            <Route path="/dark-kitchen" element={<Inicio/>}/>
+            <Route path='dark-kitchen/Bebidas' element={<Bebidas/>}/>
+            <Route path='dark-kitchen/ComidaChina' element={<ComidaChina/>}/>
+            <Route path='dark-kitchen/ComidaMexicana' element={<ComidaMexicana/>}/>
+            <Route path='dark-kitchen/ComidaRapida' element={<ComidaRapida/>}/>
+            <Route path='dark-kitchen/Ensaladas' element={<Ensaladas/>}/>
+            <Route path='dark-kitchen/Paquetes' element={<Paquetes/>}/>
 
-                <Route path='*' element={ <Navigate to="/dark-kitchen" /> }/>
-            </Routes>
-        </>
+            <Route path='*' element={<Navigate to="/dark-kitchen"/>}/>
+        </Routes>
     )
 }
--- src/components/TopComponent.jsx
+++ src/components/TopComponent.jsx
@@ -4,74 +4,73 @@ export const TopComponent = () => {
 
     const navigate = useNavigate();
     return (
-        <>
-            <ul className="opciones">
-                <li className="btn-menu">
-                    <label htmlFor="btn-menu" className="icon-menu">
-                            <span className="material-symbols-outlined">
-                                menu
-                            </span>
-                    </label>
-                </li>
-
-                <li className="nombre">
-                    <button
-                        onClick={() => navigate('/')}
-                        style={{background: 'none', border: 'none', cursor: 'pointer', padding: 0}}
-                    >
-                        Cocina
-                        <strong>Fantasma</strong>
-                        <img src="../Imagenes/Inicio/cubiertos.png" alt="" width="50"/>
-                    </button>
-                </li>
-
-                <li
-                    className="ubicacion"
-                    onClick={() => {
-                    }}
-                    style={{cursor: 'pointer'}}
-                >
-                    Ubicacion
-                </li>
-
-                <li className="buscador">
-                    <input type="text" name="hola" placeholder="Que desea comer?"/>
-
+        <ul className="opciones">
+            <li className="btn-menu">
+                <label htmlFor="btn-menu" className="icon-menu">
                     <span className="material-symbols-outlined">
-                            search
-                        </span>
-                </li>
+                        menu
+                    </span>
+                </label>
+            </li>
 
-                <li
-                    className="carrito"
-                    onClick={() => {
-                        // Lógica para abrir el carrito
-                    }}
-                    style={{cursor: 'pointer'}}
+            <li className="nombre">
+                <button
+                    onClick={() => navigate('/')}
+                    style={{background: 'none', border: 'none', cursor: 'pointer', padding: 0}}
                 >
-                    <button
-                        style={{background: 'none', border: 'none', cursor: 'pointer', padding: 0}}
-                    >
-                            <span className="material-symbols-outlined">
-                             shopping_cart
-                             </span>
-                        0 en el carro
-                    </button>
-                </li>
+                    Cocina{' '}
+                    <strong>Fantasma</strong>
+                    <img src="../Imagenes/Inicio/cubiertos.png" alt="" width="50"/>
+                </button>
+            </li>
 
-                <li className="ruedita">
-                    <button
-                        onClick={() => { /* Lógica para abrir opciones} */
-                        }}
-                        style={{background: 'none', border: 'none', cursor: 'pointer', padding: 0}}
-                    >
-                            <span className="material-symbols-outlined">
-                                settings
-                            </span>
-                        <p>Opciones</p>
-                    </button>
-                </li>
-            </ul>
-        </>
+            <li
+                className="ubicacion"
+                onClick={() => {}}
+                style={{cursor: 'pointer'}}
+            >
+                Ubicacion
+            </li>
+
+            <li className="buscador">
+                <input type="text" name="hola" placeholder="Que desea comer?"/>
+
+                <span className="material-symbols-outlined">
+                    search
+                </span>
+            </li>
+
+            <li
+                className="carrito"
+                onClick={() => {
+                    // Lógica para abrir el carrito
+                }}
+                onKeyDown={() => { /* Lógica para abrir el carrito} */
+                }}
+                style={{cursor: 'pointer'}}
+            >
+                <button
+                    style={{background: 'none', border: 'none', cursor: 'pointer', padding: 0}}
+                >
+                    <span className="material-symbols-outlined">
+                        shopping_cart
+                    </span>
+                    0 en el carro
+                </button>
+            </li>
+
+            <li className="ruedita">
+                <button
+                    onClick={() => { /* Lógica para abrir opciones} */
+                    }}
+                    style={{background: 'none', border: 'none', cursor: 'pointer', padding: 0}}
+                >
+                    <span className="material-symbols-outlined">
+                        settings
+                    </span>
+                    <p>Opciones</p>
+                </button>
+            </li>
+        </ul>
     )
 }
